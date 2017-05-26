V-Sekai-fire/multiplayer-fabric-godot

GDScript bindings for various missing Mesh functions.
Fixes #8041.

scene/resources/mesh.cpp
@@ -982,10 +982,15 @@ void Mesh::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("surface_get_material:Material", "surf_idx"), &Mesh::surface_get_material);
 	ClassDB::bind_method(D_METHOD("surface_set_name", "surf_idx", "name"), &Mesh::surface_set_name);
 	ClassDB::bind_method(D_METHOD("surface_get_name", "surf_idx"), &Mesh::surface_get_name);
+	ClassDB::bind_method(D_METHOD("create_trimesh_shape:Shape"), &Mesh::create_trimesh_shape);
+	ClassDB::bind_method(D_METHOD("create_convex_shape:Shape"), &Mesh::create_convex_shape);
+	ClassDB::bind_method(D_METHOD("create_outline:Mesh", "margin"), &Mesh::create_outline);
 	ClassDB::bind_method(D_METHOD("center_geometry"), &Mesh::center_geometry);
 	ClassDB::set_method_flags(get_class_static(), _scs_create("center_geometry"), METHOD_FLAGS_DEFAULT | METHOD_FLAG_EDITOR);
 	ClassDB::bind_method(D_METHOD("regen_normalmaps"), &Mesh::regen_normalmaps);
 	ClassDB::set_method_flags(get_class_static(), _scs_create("regen_normalmaps"), METHOD_FLAGS_DEFAULT | METHOD_FLAG_EDITOR);
+	ClassDB::bind_method(D_METHOD("get_faces"), &Mesh::get_faces);
+	ClassDB::bind_method(D_METHOD("generate_triangle_mesh:TriangleMesh"), &Mesh::generate_triangle_mesh);
 
 	ClassDB::bind_method(D_METHOD("set_custom_aabb", "aabb"), &Mesh::set_custom_aabb);
 	ClassDB::bind_method(D_METHOD("get_custom_aabb"), &Mesh::get_custom_aabb);

KleirRampage45/redot-engine

(cherry picked from commit 12c933de561d318549ee10ab2c0d6ccc34cab6b0)

scene/resources/mesh.cpp
@@ -919,10 +919,15 @@ void Mesh::_bind_methods() {
 	ObjectTypeDB::bind_method(_MD("surface_get_material:Material", "surf_idx"), &Mesh::surface_get_material);
 	ObjectTypeDB::bind_method(_MD("surface_set_name", "surf_idx", "name"), &Mesh::surface_set_name);
 	ObjectTypeDB::bind_method(_MD("surface_get_name", "surf_idx"), &Mesh::surface_get_name);
+	ObjectTypeDB::bind_method(_MD("create_trimesh_shape:Shape"), &Mesh::create_trimesh_shape);
+	ObjectTypeDB::bind_method(_MD("create_convex_shape:Shape"), &Mesh::create_convex_shape);
+	ObjectTypeDB::bind_method(_MD("create_outline:Mesh", "margin"), &Mesh::create_outline);
 	ObjectTypeDB::bind_method(_MD("center_geometry"), &Mesh::center_geometry);
 	ObjectTypeDB::set_method_flags(get_type_static(), _SCS("center_geometry"), METHOD_FLAGS_DEFAULT | METHOD_FLAG_EDITOR);
 	ObjectTypeDB::bind_method(_MD("regen_normalmaps"), &Mesh::regen_normalmaps);
 	ObjectTypeDB::set_method_flags(get_type_static(), _SCS("regen_normalmaps"), METHOD_FLAGS_DEFAULT | METHOD_FLAG_EDITOR);
+	ObjectTypeDB::bind_method(_MD("get_faces"), &Mesh::get_faces);
+	ObjectTypeDB::bind_method(_MD("generate_triangle_mesh:TriangleMesh"), &Mesh::generate_triangle_mesh);
 
 	ObjectTypeDB::bind_method(_MD("set_custom_aabb", "aabb"), &Mesh::set_custom_aabb);
 	ObjectTypeDB::bind_method(_MD("get_custom_aabb"), &Mesh::get_custom_aabb);