V-Sekai-fire/multiplayer-fabric-godot

Bindings: Fix Object::get_meta return type.
The method was shown as void in the docs.

core/object.cpp
@@ -1655,7 +1655,7 @@ void Object::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_script:Script"), &Object::get_script);
 
 	ClassDB::bind_method(D_METHOD("set_meta", "name", "value"), &Object::set_meta);
-	ClassDB::bind_method(D_METHOD("get_meta", "name", "value"), &Object::get_meta);
+	ClassDB::bind_method(D_METHOD("get_meta:Variant", "name", "value"), &Object::get_meta);
 	ClassDB::bind_method(D_METHOD("has_meta", "name"), &Object::has_meta);
 	ClassDB::bind_method(D_METHOD("get_meta_list"), &Object::_get_meta_list_bind);
 

KleirRampage45/redot-engine

(cherry picked from commit afa1ab0ed169ba12a9ac2098f234f05b3da15b67)

core/object.cpp
@@ -1538,7 +1538,7 @@ void Object::_bind_methods() {
 	ObjectTypeDB::bind_method(_MD("get_script:Script"), &Object::get_script);
 
 	ObjectTypeDB::bind_method(_MD("set_meta", "name", "value"), &Object::set_meta);
-	ObjectTypeDB::bind_method(_MD("get_meta", "name", "value"), &Object::get_meta);
+	ObjectTypeDB::bind_method(_MD("get_meta:Variant", "name", "value"), &Object::get_meta);
 	ObjectTypeDB::bind_method(_MD("has_meta", "name"), &Object::has_meta);
 	ObjectTypeDB::bind_method(_MD("get_meta_list"), &Object::_get_meta_list_bind);
 