Chaosus/godot

Continuation of work on visual particles system

scene/register_scene_types.cpp
@@ -175,6 +175,7 @@
 #include "scene/resources/video_stream.h"
 #include "scene/resources/visual_shader.h"
 #include "scene/resources/visual_shader_nodes.h"
+#include "scene/resources/visual_shader_particle_nodes.h"
 #include "scene/resources/visual_shader_sdf_nodes.h"
 #include "scene/resources/world_2d.h"
 #include "scene/resources/world_3d.h"
@@ -602,6 +603,17 @@ void register_scene_types() {
 	ClassDB::register_class<VisualShaderNodeTextureSDF>();
 	ClassDB::register_class<VisualShaderNodeTextureSDFNormal>();
 	ClassDB::register_class<VisualShaderNodeSDFRaymarch>();
+
+	ClassDB::register_class<VisualShaderNodeParticleOutput>();
+	ClassDB::register_virtual_class<VisualShaderNodeParticleEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticlePointEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleSphereEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleBoxEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleRingEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleConeVelocity>();
+	ClassDB::register_class<VisualShaderNodeParticleRandomness>();
+	ClassDB::register_class<VisualShaderNodeParticleAccelerator>();
+	ClassDB::register_class<VisualShaderNodeParticleEmit>();
 
 	ClassDB::register_class<ShaderMaterial>();
 	ClassDB::register_virtual_class<CanvasItem>();

KleirRampage45/redot-engine

scene/register_scene_types.cpp
@@ -183,6 +183,7 @@
 #include "scene/resources/video_stream.h"
 #include "scene/resources/visual_shader.h"
 #include "scene/resources/visual_shader_nodes.h"
+#include "scene/resources/visual_shader_particle_nodes.h"
 #include "scene/resources/visual_shader_sdf_nodes.h"
 #include "scene/resources/world_2d.h"
 #include "scene/resources/world_3d.h"
@@ -612,6 +613,17 @@ void register_scene_types() {
 	ClassDB::register_class<VisualShaderNodeTextureSDF>();
 	ClassDB::register_class<VisualShaderNodeTextureSDFNormal>();
 	ClassDB::register_class<VisualShaderNodeSDFRaymarch>();
+
+	ClassDB::register_class<VisualShaderNodeParticleOutput>();
+	ClassDB::register_virtual_class<VisualShaderNodeParticleEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleSphereEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleBoxEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleRingEmitter>();
+	ClassDB::register_class<VisualShaderNodeParticleMultiplyByAxisAngle>();
+	ClassDB::register_class<VisualShaderNodeParticleConeVelocity>();
+	ClassDB::register_class<VisualShaderNodeParticleRandomness>();
+	ClassDB::register_class<VisualShaderNodeParticleAccelerator>();
+	ClassDB::register_class<VisualShaderNodeParticleEmit>();
 
 	ClassDB::register_class<ShaderMaterial>();
 	ClassDB::register_virtual_class<CanvasItem>();