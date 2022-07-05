HEAVYPOLY/godot

Add full support for Android scoped storage.
This was done by refactoring directory and file access handling for the Android platform so that any general filesystem access type go through the Android layer.
This allows us to validate whether the access is unrestricted, or whether it falls under scoped storage and thus act appropriately.

# Conflicts:
#	platform/android/export/export.cpp

platform/android/file_access_filesystem_jandroid.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  file_access_android.h                                                */
+/*  file_access_filesystem_jandroid.h                                    */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,57 +28,69 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef FILE_ACCESS_ANDROID_H
-#define FILE_ACCESS_ANDROID_H
+#ifndef FILE_ACCESS_FILESYSTEM_JANDROID_H
+#define FILE_ACCESS_FILESYSTEM_JANDROID_H
 
 #include "core/os/file_access.h"
-#include <android/asset_manager.h>
-#include <android/log.h>
-#include <stdio.h>
+#include "java_godot_lib_jni.h"
 
-class FileAccessAndroid : public FileAccess {
-	mutable AAsset *a;
-	mutable uint64_t len;
-	mutable uint64_t pos;
-	mutable bool eof;
+class FileAccessFilesystemJAndroid : public FileAccess {
+	static jobject file_access_handler;
+	static jclass cls;
+
+	static jmethodID _file_open;
+	static jmethodID _file_get_size;
+	static jmethodID _file_seek;
+	static jmethodID _file_seek_end;
+	static jmethodID _file_tell;
+	static jmethodID _file_eof;
+	static jmethodID _file_read;
+	static jmethodID _file_write;
+	static jmethodID _file_flush;
+	static jmethodID _file_close;
+	static jmethodID _file_exists;
+	static jmethodID _file_last_modified;
+
+	int id;
 	String absolute_path;
 	String path_src;
 
 public:
-	static AAssetManager *asset_manager;
-
-	virtual Error _open(const String &p_path, int p_mode_flags); ///< open a file
-	virtual void close(); ///< close a file
-	virtual bool is_open() const; ///< true when file is open
+	virtual Error _open(const String &p_path, int p_mode_flags) override; ///< open a file
+	virtual void close() override; ///< close a file
+	virtual bool is_open() const override; ///< true when file is open
 
 	/// returns the path for the current open file
-	virtual String get_path() const;
+	virtual String get_path() const override;
 	/// returns the absolute path for the current open file
-	virtual String get_path_absolute() const;
+	virtual String get_path_absolute() const override;
 
-	virtual void seek(uint64_t p_position); ///< seek to a given position
-	virtual void seek_end(int64_t p_position = 0); ///< seek from the end of file
-	virtual uint64_t get_position() const; ///< get position in the file
-	virtual uint64_t get_len() const; ///< get size of the file
+	virtual void seek(uint64_t p_position) override; ///< seek to a given position
+	virtual void seek_end(int64_t p_position = 0) override; ///< seek from the end of file
+	virtual uint64_t get_position() const override; ///< get position in the file
+	virtual uint64_t get_len() const override; ///< get size of the file
 
-	virtual bool eof_reached() const; ///< reading passed EOF
+	virtual bool eof_reached() const override; ///< reading passed EOF
 
-	virtual uint8_t get_8() const; ///< get a byte
-	virtual uint64_t get_buffer(uint8_t *p_dst, uint64_t p_length) const;
+	virtual uint8_t get_8() const override; ///< get a byte
+	virtual uint64_t get_buffer(uint8_t *p_dst, uint64_t p_length) const override;
 
-	virtual Error get_error() const; ///< get last error
+	virtual Error get_error() const override; ///< get last error
 
-	virtual void flush();
-	virtual void store_8(uint8_t p_dest); ///< store a byte
+	virtual void flush() override;
+	virtual void store_8(uint8_t p_dest) override; ///< store a byte
+	virtual void store_buffer(const uint8_t *p_src, uint64_t p_length) override;
 
-	virtual bool file_exists(const String &p_path); ///< return true if a file exists
+	virtual bool file_exists(const String &p_path) override; ///< return true if a file exists
 
-	virtual uint64_t _get_modified_time(const String &p_file) { return 0; }
-	virtual uint32_t _get_unix_permissions(const String &p_file) { return 0; }
-	virtual Error _set_unix_permissions(const String &p_file, uint32_t p_permissions) { return FAILED; }
+	static void setup(jobject p_file_access_handler);
 
-	FileAccessAndroid();
-	~FileAccessAndroid();
+	virtual uint64_t _get_modified_time(const String &p_file) override;
+	virtual uint32_t _get_unix_permissions(const String &p_file) override { return 0; }
+	virtual Error _set_unix_permissions(const String &p_file, uint32_t p_permissions) override { return FAILED; }
+
+	FileAccessFilesystemJAndroid();
+	~FileAccessFilesystemJAndroid();
 };
 
-#endif // FILE_ACCESS_ANDROID_H
+#endif // FILE_ACCESS_FILESYSTEM_JANDROID_H

KleirRampage45/redot-engine

platform/android/file_access_filesystem_jandroid.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  file_access_android.h                                                */
+/*  file_access_filesystem_jandroid.h                                    */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,57 +28,70 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef FILE_ACCESS_ANDROID_H
-#define FILE_ACCESS_ANDROID_H
+#ifndef FILE_ACCESS_FILESYSTEM_JANDROID_H
+#define FILE_ACCESS_FILESYSTEM_JANDROID_H
 
 #include "core/io/file_access.h"
-#include <android/asset_manager.h>
-#include <android/log.h>
-#include <stdio.h>
+#include "java_godot_lib_jni.h"
 
-class FileAccessAndroid : public FileAccess {
-	mutable AAsset *asset = nullptr;
-	mutable uint64_t len = 0;
-	mutable uint64_t pos = 0;
-	mutable bool eof = false;
+class FileAccessFilesystemJAndroid : public FileAccess {
+	static jobject file_access_handler;
+	static jclass cls;
+
+	static jmethodID _file_open;
+	static jmethodID _file_get_size;
+	static jmethodID _file_seek;
+	static jmethodID _file_seek_end;
+	static jmethodID _file_tell;
+	static jmethodID _file_eof;
+	static jmethodID _file_read;
+	static jmethodID _file_write;
+	static jmethodID _file_flush;
+	static jmethodID _file_close;
+	static jmethodID _file_exists;
+	static jmethodID _file_last_modified;
+
+	int id;
 	String absolute_path;
 	String path_src;
 
-	void _close();
+	void _close(); ///< close a file
 
 public:
-	static AAssetManager *asset_manager;
-
-	virtual Error _open(const String &p_path, int p_mode_flags); // open a file
-	virtual bool is_open() const; // true when file is open
+	virtual Error _open(const String &p_path, int p_mode_flags) override; ///< open a file
+	virtual bool is_open() const override; ///< true when file is open
 
 	/// returns the path for the current open file
-	virtual String get_path() const;
+	virtual String get_path() const override;
 	/// returns the absolute path for the current open file
-	virtual String get_path_absolute() const;
+	virtual String get_path_absolute() const override;
 
-	virtual void seek(uint64_t p_position); // seek to a given position
-	virtual void seek_end(int64_t p_position = 0); // seek from the end of file
-	virtual uint64_t get_position() const; // get position in the file
-	virtual uint64_t get_length() const; // get size of the file
+	virtual void seek(uint64_t p_position) override; ///< seek to a given position
+	virtual void seek_end(int64_t p_position = 0) override; ///< seek from the end of file
+	virtual uint64_t get_position() const override; ///< get position in the file
+	virtual uint64_t get_length() const override; ///< get size of the file
 
-	virtual bool eof_reached() const; // reading passed EOF
+	virtual bool eof_reached() const override; ///< reading passed EOF
 
-	virtual uint8_t get_8() const; // get a byte
-	virtual uint64_t get_buffer(uint8_t *p_dst, uint64_t p_length) const;
+	virtual uint8_t get_8() const override; ///< get a byte
+	virtual uint64_t get_buffer(uint8_t *p_dst, uint64_t p_length) const override;
 
-	virtual Error get_error() const; // get last error
+	virtual Error get_error() const override; ///< get last error
 
-	virtual void flush();
-	virtual void store_8(uint8_t p_dest); // store a byte
+	virtual void flush() override;
+	virtual void store_8(uint8_t p_dest) override; ///< store a byte
+	virtual void store_buffer(const uint8_t *p_src, uint64_t p_length) override;
 
-	virtual bool file_exists(const String &p_path); // return true if a file exists
+	virtual bool file_exists(const String &p_path) override; ///< return true if a file exists
 
-	virtual uint64_t _get_modified_time(const String &p_file) { return 0; }
-	virtual uint32_t _get_unix_permissions(const String &p_file) { return 0; }
-	virtual Error _set_unix_permissions(const String &p_file, uint32_t p_permissions) { return FAILED; }
+	static void setup(jobject p_file_access_handler);
 
-	~FileAccessAndroid();
+	virtual uint64_t _get_modified_time(const String &p_file) override;
+	virtual uint32_t _get_unix_permissions(const String &p_file) override { return 0; }
+	virtual Error _set_unix_permissions(const String &p_file, uint32_t p_permissions) override { return FAILED; }
+
+	FileAccessFilesystemJAndroid();
+	~FileAccessFilesystemJAndroid();
 };
 
-#endif // FILE_ACCESS_ANDROID_H
+#endif // FILE_ACCESS_FILESYSTEM_JANDROID_H