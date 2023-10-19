Rindbee/godot

Clear the previously set state when configuring for a new scene root node
Saving a subscene causes the main scene to be re-instantiated. And the resource
instance in the main scene will be reused when the main scene is re-instantiated.
So for resources with `resource_local_to_scene` enabled, resetting state may be
necessary (at least for `ViewportTexture`).

core/io/resource.h
@@ -86,6 +86,8 @@ protected:
 	void _set_path(const String &p_path);
 	void _take_over_path(const String &p_path);
 
+	virtual void reset_setup();
+
 public:
 	static Node *(*_get_local_scene_func)(); //used by editor
 	static void (*_update_configuration_warning)(); //used by editor

KleirRampage45/redot-engine

(cherry picked from commit ad4db58440c3222e1baf39a28822ef6975751914)

core/io/resource.h
@@ -86,6 +86,8 @@ protected:
 	void _set_path(const String &p_path);
 	void _take_over_path(const String &p_path);
 
+	virtual void reset_local_to_scene();
+
 public:
 	static Node *(*_get_local_scene_func)(); //used by editor
 	static void (*_update_configuration_warning)(); //used by editor