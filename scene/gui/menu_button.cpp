godotengine/godot

Add PropertyListHelper in all simple cases

scene/gui/menu_button.cpp
@@ -153,52 +153,23 @@ void MenuButton::_notification(int p_what) {
 }
 
 bool MenuButton::_set(const StringName &p_name, const Variant &p_value) {
-	Vector<String> components = String(p_name).split("/", true, 2);
-	if (components.size() >= 2 && components[0] == "popup") {
+	const String sname = p_name;
+	if (property_helper.validate_property(sname)) {
 		bool valid;
-		popup->set(String(p_name).trim_prefix("popup/"), p_value, &valid);
+		popup->set(sname.trim_prefix("popup/"), p_value, &valid);
 		return valid;
 	}
 	return false;
 }
 
 bool MenuButton::_get(const StringName &p_name, Variant &r_ret) const {
-	Vector<String> components = String(p_name).split("/", true, 2);
-	if (components.size() >= 2 && components[0] == "popup") {
+	const String sname = p_name;
+	if (property_helper.validate_property(sname)) {
 		bool valid;
-		r_ret = popup->get(String(p_name).trim_prefix("popup/"), &valid);
+		r_ret = popup->get(sname.trim_prefix("popup/"), &valid);
 		return valid;
 	}
 	return false;
-}
-
-void MenuButton::_get_property_list(List<PropertyInfo> *p_list) const {
-	for (int i = 0; i < popup->get_item_count(); i++) {
-		p_list->push_back(PropertyInfo(Variant::STRING, vformat("popup/item_%d/text", i)));
-
-		PropertyInfo pi = PropertyInfo(Variant::OBJECT, vformat("popup/item_%d/icon", i), PROPERTY_HINT_RESOURCE_TYPE, "Texture2D");
-		pi.usage &= ~(popup->get_item_icon(i).is_null() ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::INT, vformat("popup/item_%d/checkable", i), PROPERTY_HINT_ENUM, "No,As Checkbox,As Radio Button");
-		pi.usage &= ~(!popup->is_item_checkable(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::BOOL, vformat("popup/item_%d/checked", i));
-		pi.usage &= ~(!popup->is_item_checked(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::INT, vformat("popup/item_%d/id", i), PROPERTY_HINT_RANGE, "0,10,1,or_greater");
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::BOOL, vformat("popup/item_%d/disabled", i));
-		pi.usage &= ~(!popup->is_item_disabled(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::BOOL, vformat("popup/item_%d/separator", i));
-		pi.usage &= ~(!popup->is_item_separator(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-	}
 }
 
 void MenuButton::_bind_methods() {
@@ -215,6 +186,17 @@ void MenuButton::_bind_methods() {
 	ADD_ARRAY_COUNT("Items", "item_count", "set_item_count", "get_item_count", "popup/item_");
 
 	ADD_SIGNAL(MethodInfo("about_to_popup"));
+
+	PopupMenu::Item defaults(true);
+
+	base_property_helper.set_prefix("popup/item_");
+	base_property_helper.register_property(PropertyInfo(Variant::STRING, "text"), defaults.text);
+	base_property_helper.register_property(PropertyInfo(Variant::OBJECT, "icon", PROPERTY_HINT_RESOURCE_TYPE, "Texture2D"), defaults.icon);
+	base_property_helper.register_property(PropertyInfo(Variant::INT, "checkable", PROPERTY_HINT_ENUM, "No,As Checkbox,As Radio Button"), defaults.checkable_type);
+	base_property_helper.register_property(PropertyInfo(Variant::BOOL, "checked"), defaults.checked);
+	base_property_helper.register_property(PropertyInfo(Variant::INT, "id", PROPERTY_HINT_RANGE, "0,10,1,or_greater"), defaults.id);
+	base_property_helper.register_property(PropertyInfo(Variant::BOOL, "disabled"), defaults.disabled);
+	base_property_helper.register_property(PropertyInfo(Variant::BOOL, "separator"), defaults.separator);
 }
 
 void MenuButton::set_disable_shortcuts(bool p_disabled) {
@@ -235,6 +217,8 @@ MenuButton::MenuButton(const String &p_text) :
 	add_child(popup, false, INTERNAL_MODE_FRONT);
 	popup->connect("about_to_popup", callable_mp(this, &MenuButton::_popup_visibility_changed).bind(true));
 	popup->connect("popup_hide", callable_mp(this, &MenuButton::_popup_visibility_changed).bind(false));
+
+	property_helper.setup_for_instance(base_property_helper, this);
 }
 
 MenuButton::~MenuButton() {

scene/property_list_helper.cpp
@@ -31,7 +31,7 @@
 #include "property_list_helper.h"
 
 const PropertyListHelper::Property *PropertyListHelper::_get_property(const String &p_property, int *r_index) const {
-	const Vector<String> components = p_property.split("/", true, 2);
+	const Vector<String> components = p_property.rsplit("/", true, 1);
 	if (components.size() < 2 || !components[0].begins_with(prefix)) {
 		return nullptr;
 	}
@@ -48,21 +48,38 @@ const PropertyListHelper::Property *PropertyListHelper::_get_property(const Stri
 }
 
 void PropertyListHelper::_call_setter(const MethodBind *p_setter, int p_index, const Variant &p_value) const {
+	DEV_ASSERT(p_setter);
 	Variant args[] = { p_index, p_value };
 	const Variant *argptrs[] = { &args[0], &args[1] };
 	Callable::CallError ce;
 	p_setter->call(object, argptrs, 2, ce);
 }
 
-Variant PropertyListHelper::_call_getter(const MethodBind *p_getter, int p_index) const {
+Variant PropertyListHelper::_call_getter(const Property *p_property, int p_index) const {
+	if (!p_property->getter) {
+		return object->get(prefix + itos(p_index) + "/" + p_property->info.name);
+	}
+
 	Callable::CallError ce;
 	Variant args[] = { p_index };
 	const Variant *argptrs[] = { &args[0] };
-	return p_getter->call(object, argptrs, 1, ce);
+	return p_property->getter->call(object, argptrs, 1, ce);
 }
 
 void PropertyListHelper::set_prefix(const String &p_prefix) {
 	prefix = p_prefix;
+}
+
+void PropertyListHelper::register_property(const PropertyInfo &p_info, const Variant &p_default) {
+	Property property;
+	property.info = p_info;
+	property.default_value = p_default;
+
+	property_list[p_info.name] = property;
+}
+
+bool PropertyListHelper::is_initialized() const {
+	return !property_list.is_empty();
 }
 
 void PropertyListHelper::setup_for_instance(const PropertyListHelper &p_base, Object *p_object) {
@@ -71,13 +88,33 @@ void PropertyListHelper::setup_for_instance(const PropertyListHelper &p_base, Ob
 	object = p_object;
 }
 
+bool PropertyListHelper::validate_property(const String &p_property, int *r_index) const {
+	const Vector<String> components = p_property.rsplit("/", true, 1);
+	if (components.size() < 2 || !components[0].begins_with(prefix)) {
+		return false;
+	}
+
+	{
+		const String index_string = components[0].trim_prefix(prefix);
+		if (!index_string.is_valid_int()) {
+			return false;
+		}
+
+		if (r_index) {
+			*r_index = index_string.to_int();
+		}
+	}
+
+	return property_list.has(components[1]);
+}
+
 void PropertyListHelper::get_property_list(List<PropertyInfo> *p_list, int p_count) const {
 	for (int i = 0; i < p_count; i++) {
 		for (const KeyValue<String, Property> &E : property_list) {
 			const Property &property = E.value;
 
 			PropertyInfo info = property.info;
-			if (_call_getter(property.getter, i) == property.default_value) {
+			if (_call_getter(&property, i) == property.default_value) {
 				info.usage &= (~PROPERTY_USAGE_STORAGE);
 			}
 
@@ -92,7 +129,7 @@ bool PropertyListHelper::property_get_value(const String &p_property, Variant &r
 	const Property *property = _get_property(p_property, &index);
 
 	if (property) {
-		r_ret = _call_getter(property->getter, index);
+		r_ret = _call_getter(property, index);
 		return true;
 	}
 	return false;
@@ -129,8 +166,10 @@ PropertyListHelper::~PropertyListHelper() {
 	// No object = it's the main helper. Do a cleanup.
 	if (!object) {
 		for (const KeyValue<String, Property> &E : property_list) {
-			memdelete(E.value.setter);
-			memdelete(E.value.getter);
+			if (E.value.setter) {
+				memdelete(E.value.setter);
+				memdelete(E.value.getter);
+			}
 		}
 	}
 }

scene/property_list_helper.h
@@ -48,10 +48,12 @@ class PropertyListHelper {
 
 	const Property *_get_property(const String &p_property, int *r_index) const;
 	void _call_setter(const MethodBind *p_setter, int p_index, const Variant &p_value) const;
-	Variant _call_getter(const MethodBind *p_getter, int p_index) const;
+	Variant _call_getter(const Property *p_property, int p_index) const;
 
 public:
 	void set_prefix(const String &p_prefix);
+	// Register property without setter/getter. Only use when you don't need PropertyListHelper for _set/_get logic.
+	void register_property(const PropertyInfo &p_info, const Variant &p_default);
 
 	template <typename S, typename G>
 	void register_property(const PropertyInfo &p_info, const Variant &p_default, S p_setter, G p_getter) {
@@ -64,7 +66,9 @@ public:
 		property_list[p_info.name] = property;
 	}
 
+	bool is_initialized() const;
 	void setup_for_instance(const PropertyListHelper &p_base, Object *p_object);
+	bool validate_property(const String &p_property, int *r_index = nullptr) const;
 
 	void get_property_list(List<PropertyInfo> *p_list, int p_count) const;
 	bool property_get_value(const String &p_property, Variant &r_ret) const;

KleirRampage45/redot-engine

scene/gui/menu_button.cpp
@@ -153,52 +153,23 @@ void MenuButton::_notification(int p_what) {
 }
 
 bool MenuButton::_set(const StringName &p_name, const Variant &p_value) {
-	Vector<String> components = String(p_name).split("/", true, 2);
-	if (components.size() >= 2 && components[0] == "popup") {
+	const String sname = p_name;
+	if (property_helper.is_property_valid(sname)) {
 		bool valid;
-		popup->set(String(p_name).trim_prefix("popup/"), p_value, &valid);
+		popup->set(sname.trim_prefix("popup/"), p_value, &valid);
 		return valid;
 	}
 	return false;
 }
 
 bool MenuButton::_get(const StringName &p_name, Variant &r_ret) const {
-	Vector<String> components = String(p_name).split("/", true, 2);
-	if (components.size() >= 2 && components[0] == "popup") {
+	const String sname = p_name;
+	if (property_helper.is_property_valid(sname)) {
 		bool valid;
-		r_ret = popup->get(String(p_name).trim_prefix("popup/"), &valid);
+		r_ret = popup->get(sname.trim_prefix("popup/"), &valid);
 		return valid;
 	}
 	return false;
-}
-
-void MenuButton::_get_property_list(List<PropertyInfo> *p_list) const {
-	for (int i = 0; i < popup->get_item_count(); i++) {
-		p_list->push_back(PropertyInfo(Variant::STRING, vformat("popup/item_%d/text", i)));
-
-		PropertyInfo pi = PropertyInfo(Variant::OBJECT, vformat("popup/item_%d/icon", i), PROPERTY_HINT_RESOURCE_TYPE, "Texture2D");
-		pi.usage &= ~(popup->get_item_icon(i).is_null() ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::INT, vformat("popup/item_%d/checkable", i), PROPERTY_HINT_ENUM, "No,As Checkbox,As Radio Button");
-		pi.usage &= ~(!popup->is_item_checkable(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::BOOL, vformat("popup/item_%d/checked", i));
-		pi.usage &= ~(!popup->is_item_checked(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::INT, vformat("popup/item_%d/id", i), PROPERTY_HINT_RANGE, "0,10,1,or_greater");
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::BOOL, vformat("popup/item_%d/disabled", i));
-		pi.usage &= ~(!popup->is_item_disabled(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-
-		pi = PropertyInfo(Variant::BOOL, vformat("popup/item_%d/separator", i));
-		pi.usage &= ~(!popup->is_item_separator(i) ? PROPERTY_USAGE_STORAGE : 0);
-		p_list->push_back(pi);
-	}
 }
 
 void MenuButton::_bind_methods() {
@@ -215,6 +186,17 @@ void MenuButton::_bind_methods() {
 	ADD_ARRAY_COUNT("Items", "item_count", "set_item_count", "get_item_count", "popup/item_");
 
 	ADD_SIGNAL(MethodInfo("about_to_popup"));
+
+	PopupMenu::Item defaults(true);
+
+	base_property_helper.set_prefix("popup/item_");
+	base_property_helper.register_property(PropertyInfo(Variant::STRING, "text"), defaults.text);
+	base_property_helper.register_property(PropertyInfo(Variant::OBJECT, "icon", PROPERTY_HINT_RESOURCE_TYPE, "Texture2D"), defaults.icon);
+	base_property_helper.register_property(PropertyInfo(Variant::INT, "checkable", PROPERTY_HINT_ENUM, "No,As Checkbox,As Radio Button"), defaults.checkable_type);
+	base_property_helper.register_property(PropertyInfo(Variant::BOOL, "checked"), defaults.checked);
+	base_property_helper.register_property(PropertyInfo(Variant::INT, "id", PROPERTY_HINT_RANGE, "0,10,1,or_greater"), defaults.id);
+	base_property_helper.register_property(PropertyInfo(Variant::BOOL, "disabled"), defaults.disabled);
+	base_property_helper.register_property(PropertyInfo(Variant::BOOL, "separator"), defaults.separator);
 }
 
 void MenuButton::set_disable_shortcuts(bool p_disabled) {
@@ -235,6 +217,8 @@ MenuButton::MenuButton(const String &p_text) :
 	add_child(popup, false, INTERNAL_MODE_FRONT);
 	popup->connect("about_to_popup", callable_mp(this, &MenuButton::_popup_visibility_changed).bind(true));
 	popup->connect("popup_hide", callable_mp(this, &MenuButton::_popup_visibility_changed).bind(false));
+
+	property_helper.setup_for_instance(base_property_helper, this);
 }
 
 MenuButton::~MenuButton() {

scene/property_list_helper.cpp
@@ -31,7 +31,7 @@
 #include "property_list_helper.h"
 
 const PropertyListHelper::Property *PropertyListHelper::_get_property(const String &p_property, int *r_index) const {
-	const Vector<String> components = p_property.split("/", true, 2);
+	const Vector<String> components = p_property.rsplit("/", true, 1);
 	if (components.size() < 2 || !components[0].begins_with(prefix)) {
 		return nullptr;
 	}
@@ -48,21 +48,38 @@ const PropertyListHelper::Property *PropertyListHelper::_get_property(const Stri
 }
 
 void PropertyListHelper::_call_setter(const MethodBind *p_setter, int p_index, const Variant &p_value) const {
+	DEV_ASSERT(p_setter);
 	Variant args[] = { p_index, p_value };
 	const Variant *argptrs[] = { &args[0], &args[1] };
 	Callable::CallError ce;
 	p_setter->call(object, argptrs, 2, ce);
 }
 
-Variant PropertyListHelper::_call_getter(const MethodBind *p_getter, int p_index) const {
+Variant PropertyListHelper::_call_getter(const Property *p_property, int p_index) const {
+	if (!p_property->getter) {
+		return object->get(prefix + itos(p_index) + "/" + p_property->info.name);
+	}
+
 	Callable::CallError ce;
 	Variant args[] = { p_index };
 	const Variant *argptrs[] = { &args[0] };
-	return p_getter->call(object, argptrs, 1, ce);
+	return p_property->getter->call(object, argptrs, 1, ce);
 }
 
 void PropertyListHelper::set_prefix(const String &p_prefix) {
 	prefix = p_prefix;
+}
+
+void PropertyListHelper::register_property(const PropertyInfo &p_info, const Variant &p_default) {
+	Property property;
+	property.info = p_info;
+	property.default_value = p_default;
+
+	property_list[p_info.name] = property;
+}
+
+bool PropertyListHelper::is_initialized() const {
+	return !property_list.is_empty();
 }
 
 void PropertyListHelper::setup_for_instance(const PropertyListHelper &p_base, Object *p_object) {
@@ -71,13 +88,33 @@ void PropertyListHelper::setup_for_instance(const PropertyListHelper &p_base, Ob
 	object = p_object;
 }
 
+bool PropertyListHelper::is_property_valid(const String &p_property, int *r_index) const {
+	const Vector<String> components = p_property.rsplit("/", true, 1);
+	if (components.size() < 2 || !components[0].begins_with(prefix)) {
+		return false;
+	}
+
+	{
+		const String index_string = components[0].trim_prefix(prefix);
+		if (!index_string.is_valid_int()) {
+			return false;
+		}
+
+		if (r_index) {
+			*r_index = index_string.to_int();
+		}
+	}
+
+	return property_list.has(components[1]);
+}
+
 void PropertyListHelper::get_property_list(List<PropertyInfo> *p_list, int p_count) const {
 	for (int i = 0; i < p_count; i++) {
 		for (const KeyValue<String, Property> &E : property_list) {
 			const Property &property = E.value;
 
 			PropertyInfo info = property.info;
-			if (_call_getter(property.getter, i) == property.default_value) {
+			if (_call_getter(&property, i) == property.default_value) {
 				info.usage &= (~PROPERTY_USAGE_STORAGE);
 			}
 
@@ -92,7 +129,7 @@ bool PropertyListHelper::property_get_value(const String &p_property, Variant &r
 	const Property *property = _get_property(p_property, &index);
 
 	if (property) {
-		r_ret = _call_getter(property->getter, index);
+		r_ret = _call_getter(property, index);
 		return true;
 	}
 	return false;
@@ -110,8 +147,7 @@ bool PropertyListHelper::property_set_value(const String &p_property, const Vari
 }
 
 bool PropertyListHelper::property_can_revert(const String &p_property) const {
-	int index;
-	return _get_property(p_property, &index) != nullptr;
+	return is_property_valid(p_property);
 }
 
 bool PropertyListHelper::property_get_revert(const String &p_property, Variant &r_value) const {
@@ -129,8 +165,10 @@ PropertyListHelper::~PropertyListHelper() {
 	// No object = it's the main helper. Do a cleanup.
 	if (!object) {
 		for (const KeyValue<String, Property> &E : property_list) {
-			memdelete(E.value.setter);
-			memdelete(E.value.getter);
+			if (E.value.setter) {
+				memdelete(E.value.setter);
+				memdelete(E.value.getter);
+			}
 		}
 	}
 }

scene/property_list_helper.h
@@ -48,10 +48,12 @@ class PropertyListHelper {
 
 	const Property *_get_property(const String &p_property, int *r_index) const;
 	void _call_setter(const MethodBind *p_setter, int p_index, const Variant &p_value) const;
-	Variant _call_getter(const MethodBind *p_getter, int p_index) const;
+	Variant _call_getter(const Property *p_property, int p_index) const;
 
 public:
 	void set_prefix(const String &p_prefix);
+	// Register property without setter/getter. Only use when you don't need PropertyListHelper for _set/_get logic.
+	void register_property(const PropertyInfo &p_info, const Variant &p_default);
 
 	template <typename S, typename G>
 	void register_property(const PropertyInfo &p_info, const Variant &p_default, S p_setter, G p_getter) {
@@ -64,7 +66,9 @@ public:
 		property_list[p_info.name] = property;
 	}
 
+	bool is_initialized() const;
 	void setup_for_instance(const PropertyListHelper &p_base, Object *p_object);
+	bool is_property_valid(const String &p_property, int *r_index = nullptr) const;
 
 	void get_property_list(List<PropertyInfo> *p_list, int p_count) const;
 	bool property_get_value(const String &p_property, Variant &r_ret) const;