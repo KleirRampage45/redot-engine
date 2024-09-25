Izaaqk/Godot

SCons: Add unobtrusive type hints in SCons files

SConstruct
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from misc.scripts.scons_hints import *
 
 EnsureSConsVersion(3, 1, 2)
 EnsurePythonVersion(3, 6)

KleirRampage45/redot-engine

SConstruct
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from misc.utility.scons_hints import *
 
 EnsureSConsVersion(3, 1, 2)
 EnsurePythonVersion(3, 6)